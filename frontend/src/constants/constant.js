32da1e36eefb46fef0911d8bcef1322fd6900673
rohit <rhtry.tech@gmail.com>
2023-05-13 21:19:14 +0530

    add : files

--- frontend/src/constants/constant.js
+++ frontend/src/constants/constant.js
@@ -1,3 +1,4 @@
+// pasteTheContractAddressHereAfterDeployingTheContract
 const contractAddress = "0x5097ace2ac4A9375f0EF380075Fd0A15e5c35118";
 
 import ABI_JSON from "../../../backend (solidity-hardhat)/artifacts/contracts/Voting.sol/Voting.json";
