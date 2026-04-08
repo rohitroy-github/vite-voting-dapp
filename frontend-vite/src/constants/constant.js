6db1ddd33780a1cc0ec49eab4189d7001f5c49d0
rohitroy@github <rhtry.tech@gmail.com>
2026-04-08 19:25:30 +0530

    check: final checks [working]

--- frontend-vite/src/constants/constant.js
+++ frontend-vite/src/constants/constant.js
@@ -1,5 +1,5 @@
 // pasteTheContractAddressHereAfterDeployingTheContract
-const contractAddress = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 import ABI_JSON from "../../../backend-hardhat/artifacts/contracts/VotingContract.sol/VotingContract.json";
 
